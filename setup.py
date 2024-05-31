ebfe2a7948748595344fe97d58534eee3901cc28
Bill Poulson <poulson.bill@gmail.com>
2024-05-30 23:02:59 -0400

    move to src folder

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ setup(
     name="harvest_cli",
     author="Bill Poulson",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     py_modules=["cli", "authorization", "config", "harvest_client", "util"],
     install_requires=[
-        "requests==2.26.0",
+        "requests>=2.26.0",
         "click>=8.0.1",
-        "inquirer==3.1.4",
-        "flask==3.0.3",
+        "inquirer>=3.1.4",
+        "flask>=3.0.3",
     ],
     entry_points={
         "console_scripts": [
-            "harvest=cli:cli",  # Adjusted to point to the correct module
+            "harvest=cli:cli",  # Adjusted to remove the 'src' prefix
         ],
     },
 )
